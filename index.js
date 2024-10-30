033a1b4618650abb65823b51f691baacebb66a15
milap-magar <milapmagar12@gmail.com>
2024-10-30 17:09:12 +0545

    more

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const jsonServer = require("json-server"); 
+const jsonServer = require("json-server");
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8080;
+
+const server = jsonServer.create();
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
